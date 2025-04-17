8a6e88682ab684e602e67de61d78b9cd800ea2ce
zhao qing feng <qingfengzhao@faceunity.com>
2025-04-17 16:30:02 +0800

    update 8.14.0 & update third parties

--- ThirdParty/Mac/cmakeScripts/Modules/FindOpencv.cmake
+++ ThirdParty/Mac/cmakeScripts/Modules/FindOpencv.cmake
@@ -5,22 +5,36 @@ function(FindOpencv package)
     set(${package}_INCLUDE_DIRS ${${package}_HOME}/include PARENT_SCOPE)
 	if(${CMAKE_HOST_SYSTEM_PROCESSOR} MATCHES "x86_64")
 	    set(${package}_LIBRARIES
-			${${package}_HOME}/lib/x86_64/libopencv_core.3.4.dylib
-			${${package}_HOME}/lib/x86_64/libopencv_highgui.3.4.dylib
-			${${package}_HOME}/lib/x86_64/libopencv_imgcodecs.3.4.dylib
-			${${package}_HOME}/lib/x86_64/libopencv_imgproc.3.4.dylib
-			${${package}_HOME}/lib/x86_64/libopencv_video.3.4.dylib
-			${${package}_HOME}/lib/x86_64/libopencv_videoio.3.4.dylib
+			${${package}_HOME}/lib/x86_64/libopencv_core.dylib
+			${${package}_HOME}/lib/x86_64/libopencv_highgui.dylib
+			${${package}_HOME}/lib/x86_64/libopencv_imgcodecs.dylib
+			${${package}_HOME}/lib/x86_64/libopencv_imgproc.dylib
+			${${package}_HOME}/lib/x86_64/libopencv_video.dylib
+			${${package}_HOME}/lib/x86_64/libopencv_videoio.dylib
 		PARENT_SCOPE)
 	 else()
 		set(${package}_LIBRARIES
-			${${package}_HOME}/lib/arm64/libopencv_core.3.4.dylib
-			${${package}_HOME}/lib/arm64/libopencv_highgui.3.4.dylib
-			${${package}_HOME}/lib/arm64/libopencv_imgcodecs.3.4.dylib
-			${${package}_HOME}/lib/arm64/libopencv_imgproc.3.4.dylib
-			${${package}_HOME}/lib/arm64/libopencv_video.3.4.dylib
-			${${package}_HOME}/lib/arm64/libopencv_videoio.3.4.dylib
-		PARENT_SCOPE)
+
+			# ${${package}_HOME}/lib/arm64/libopencv_core.3.4.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_highgui.3.4.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_imgcodecs.3.4.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_imgproc.3.4.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_video.3.4.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_videoio.3.4.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_core.4.5.1.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_highgui.4.5.1.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_imgcodecs.4.5.1.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_imgproc.4.5.1.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_video.4.5.1.dylib
+			# ${${package}_HOME}/lib/arm64/libopencv_videoio.4.5.1.dylib
+			${${package}_HOME}/lib/arm64/libopencv_core.dylib
+			${${package}_HOME}/lib/arm64/libopencv_highgui.dylib
+			${${package}_HOME}/lib/arm64/libopencv_imgcodecs.dylib
+			${${package}_HOME}/lib/arm64/libopencv_imgproc.dylib
+			${${package}_HOME}/lib/arm64/libopencv_video.dylib
+			${${package}_HOME}/lib/arm64/libopencv_videoio.dylib
+
+			PARENT_SCOPE)
 	endif()
 endfunction(FindOpencv)
 
